1862acb5700cc8688d090a070326a238bd00c940
Jayashree <jaya@cs.utexas.edu>
2020-04-09 11:34:09 -0700

    Audio decoder support for mint

--- dali/kernels/signal/resampling.h
+++ dali/kernels/signal/resampling.h
@@ -23,6 +23,8 @@
 #include "dali/core/small_vector.h"
 #include "dali/core/convert.h"
 #include "dali/core/static_switch.h"
+#include <cstring>
+#include <cstdlib>
 
 namespace dali {
 namespace kernels {
@@ -79,6 +81,14 @@ inline int64_t resampled_length(int64_t in_length, double in_rate, double out_ra
   return std::ceil(in_length * out_rate / in_rate);
 }
 
+inline int64_t resampled_length_cropped(int64_t in_length, double in_rate, double out_rate, int cropped_size, int scale) {
+  //int scale = std::floor(in_rate / out_rate);
+  //std::cout << "Scale : " << scale << std::endl;
+  int64_t new_length = cropped_size/scale;
+  //std::cout << "New length : " << new_length << std::endl;
+  return new_length;
+}
+
 struct Resampler {
   ResamplingWindow window;
 
@@ -127,6 +137,78 @@ struct Resampler {
       }
     }
   }
+
+
+  /**
+   * @brief Resample single-channel signal and convert to Out
+   *
+   * Calculates a range of resampled signal.
+   * The function can seamlessly resample the input and produce the result in chunks.
+   * To reuse memory and still simulate chunk processing, adjust the in/out pointers.
+   */
+  template <typename Out>
+  void Resample_Crop(
+        Out *__restrict__ out, int64_t out_begin, int64_t out_end, double out_rate,
+        const float *__restrict__ in_full, int64_t n_in, double in_rate, int crop_size, int scale) const {
+    assert(out_rate > 0 && in_rate > 0 && "Sampling rate must be positive");
+    assert(crop_size > 0 && "Crop size must be positive");
+    int64_t in_pos = 0;
+    int64_t block = 1 << 10;  // still leaves 13 significant bits for fractional part
+    if (out_rate == 0)
+        std::cout << "OUT RATE IS 0" << std::endl;
+    //int scale = in_rate / out_rate;
+    //out_end = crop_size/scale;
+    float fscale = scale;
+
+    float *__restrict__ in;
+    in = (float*) malloc (crop_size*sizeof(float));
+    //in = new float[crop_size];  
+    assert(in != nullptr && "Error allocating new memory");
+    memset(in, 0, crop_size * sizeof(float));
+    //nstd::cout << "input size="<< n_in << ", crop size=" << crop_size << std::endl;
+    if(n_in <= crop_size){
+        //Simply copy the input
+        memcpy(in, in_full, n_in*sizeof(float));
+    }
+    else{
+       //generate rand number between 0 and n_in-crop_size
+       int64_t max_limit = n_in - crop_size;
+       int64_t rand_start = rand() % max_limit;
+       //std::cout << "Random number is " << rand_start << std::endl;
+       memcpy(in, in_full + rand_start, crop_size*sizeof(float));
+    }
+    n_in = crop_size;
+    //std::cout << "new input size="<< n_in << ", out begin=" << out_begin << " out end= "<< out_end << std::endl;
+
+    for (int64_t out_block = out_begin; out_block < out_end; out_block += block) {
+      int64_t block_end = std::min(out_block + block, out_end);
+      double in_block_f = out_block * scale;
+      int64_t in_block_i = std::floor(in_block_f);
+      float in_pos = in_block_f - in_block_i;
+      const float *__restrict__ in_block_ptr = in + in_block_i;
+      for (int64_t out_pos = out_block; out_pos < block_end; out_pos++, in_pos += fscale) {
+        int i0, i1;
+        std::tie(i0, i1) = window.input_range(in_pos);
+        if (i0 + in_block_i < 0)
+          i0 = -in_block_i;
+        if (i1 + in_block_i >= n_in)
+          i1 = n_in - 1 - in_block_i;
+        float f = 0;
+        float x = i0 - in_pos;
+        for (int i = i0; i <= i1; i++, x++) {
+          assert(in_block_ptr + i >= in && in_block_ptr + i < in + n_in);
+          float w = window(x);
+          f += in_block_ptr[i] * w;
+        }
+        assert(out_pos >= out_begin && out_pos < out_end);
+        out[out_pos] = ConvertSatNorm<Out>(f);
+      }
+    }
+   free(in);
+  }
+
+
+
 
 
   /**
